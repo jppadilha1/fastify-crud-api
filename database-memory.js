6918552ac1fd93ff14d33c1e7dbb7be5e5491501
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-04-06 19:42:50 +0000

    remove some comments

--- database-memory.js
+++ database-memory.js
@@ -1,33 +1,31 @@
-import {randomUUID} from 'node:crypto' // gera sempre um id aleatório
+import { randomUUID } from "node:crypto";
 
 export class DatabaseMemory {
-    #videos = new Map()  // são outros tipos de estruturas de dados, set e map ( do Javascript), set não duplica, Map possui indexação, tipo chave, valor, para passarmos o id
+  #videos = new Map();
 
-    list() {
-        return Array.from(this.#videos.entries()).map((arrayKeyValue) => {
-            const id = arrayKeyValue[0]
-            const data = arrayKeyValue[1]
+  list() {
+    return Array.from(this.#videos.entries()).map((arrayKeyValue) => {
+      const id = arrayKeyValue[0];
+      const data = arrayKeyValue[1];
 
-            return {
-                id,
-                ...data
-            }
-        }) //array.from, transforma uma estrutura em array // entries(), retorna um array com a chave e o valor
+      return {
+        id,
+        ...data,
+      };
+    });
+  }
 
-    }
-    
-    create(treino) {
-        const idTreino = randomUUID()
+  create(treino) {
+    const idTreino = randomUUID();
 
-        this.#videos.set(idTreino, treino)
-    }
+    this.#videos.set(idTreino, treino);
+  }
 
-    update(id, newtreino) {
-        this.#videos.set(id, newtreino)
-    }
+  update(id, newtreino) {
+    this.#videos.set(id, newtreino);
+  }
 
-    delete(id) {
-        this.#videos.delete(id)
-    }
+  delete(id) {
+    this.#videos.delete(id);
+  }
 }
-
